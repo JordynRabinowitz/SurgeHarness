465c47ea22db287635d432a9df17e4f4cfbfac4c
JordynRabinowitz <jordynjr2004@gmail.com>
2026-03-03 14:34:40 -0700

    Set up GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
+const basePath = process.env.PAGES_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath,
+
+  // Optional but often helpful on static hosts:
+  trailingSlash: true,
 };
 
 export default nextConfig;
